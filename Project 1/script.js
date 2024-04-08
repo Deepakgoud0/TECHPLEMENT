3b2ac16b70a66af89f9f75ed40e36bf9ec393cfb
Deepak Goud <161675362+Deepakgoud0@users.noreply.github.com>
2024-04-08 12:14:55 +0530

    Delete Project 1 directory

--- Project 1/script.js
+++
@@ -1,9 +0,0 @@
-
-let bookNowButtonEl = document.getElementById("bookNowButton");
-let registerCardEl = document.getElementById("registerCard");
-
-
-
-bookNowButtonEl.onclick = function() {
-    registerCardEl.classList.remove("display");
-}
